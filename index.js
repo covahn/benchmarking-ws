dd16ad3771f1377b3a3f2588d2c1333d9f414b00
cov <covahn.cov@gmail.com>
2020-06-21 02:14:56 +0200

    init

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('app running');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('ws connection');
+});
+
+server.listen(3005);
