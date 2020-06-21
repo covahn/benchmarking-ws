9b39ac030ef1d0d546ef9f3d87fef812e1681d41
cov <covahn.cov@gmail.com>
2020-06-21 02:33:58 +0200

    init

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const io = require('socket.io')(server);
 
 io.on('connection', socket => {
   console.log('ws connection');
+
+  socket.on('pos', pos => {
+    socket.broadcast.emit('pos', pos);
+  });
 });
 
 server.listen(3005);
